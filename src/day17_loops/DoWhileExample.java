eab9da0255e65b4f8bf03dc0ae2af34d84a26ba8
aydinmohsunov <aydintx037@gmail.com>
2025-06-12 21:13:17 -0500

    New easy topic Loop

---
+++ src/day17_loops/DoWhileExample.java
@@ -0,0 +1,28 @@
+package day17_loops;
+
+public class DoWhileExample {
+    public static void main(String[] args) {
+        // While LOOP
+        int m = 1;
+        while (m<=10){
+
+            System.out.println("While Lopp "+m);
+            m++;
+        }
+
+        // flow
+        //   1st chek condition
+        //   if true execution code block
+
+
+        System.out.println("==================================");
+        int n = 1;
+        do {
+            System.out.println("Do hwile loop "+n);
+            n++;
+        }while (n<=10);
+        // flow
+        // 1 execute code block of DO
+        //  then chek condition
+    }
+}
